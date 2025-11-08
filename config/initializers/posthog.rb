8297a264371826f425109f46bdda9c438981878b
Juan José Mata <jjmata@jjmata.com>
2025-11-08 20:19:47 +0100

    Propagate PostHog config to view

--- config/initializers/posthog.rb
+++ config/initializers/posthog.rb
@@ -1,6 +1,10 @@
-if (api_key = ENV['POSTHOG_KEY']).present?
+Rails.configuration.x.posthog = ActiveSupport::OrderedOptions.new
+Rails.configuration.x.posthog.key = ENV['POSTHOG_KEY'].presence
+Rails.configuration.x.posthog.host = ENV.fetch('POSTHOG_HOST', 'https://app.posthog.com')
+
+if (api_key = Rails.configuration.x.posthog.key).present?
   PostHog.configure do |config|
     config.api_key = api_key
-    config.host = ENV.fetch('POSTHOG_HOST', 'https://app.posthog.com')
+    config.host = Rails.configuration.x.posthog.host
   end
 end

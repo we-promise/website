dde47f543d9126f9f82529b2623f7641ff1dc923
Juan José Mata <juanjo.mata@gmail.com>
2025-11-08 19:30:01 +0000

    Update to latest PostHog scripts

--- config/initializers/posthog.rb
+++ config/initializers/posthog.rb
@@ -1,6 +1,8 @@
+require "posthog"
+
 Rails.configuration.x.posthog = ActiveSupport::OrderedOptions.new
-Rails.configuration.x.posthog.key = ENV['POSTHOG_KEY'].presence
-Rails.configuration.x.posthog.host = ENV.fetch('POSTHOG_HOST', 'https://app.posthog.com')
+Rails.configuration.x.posthog.key = ENV["POSTHOG_KEY"].presence
+Rails.configuration.x.posthog.host = ENV.fetch("POSTHOG_HOST", "https://us.i.posthog.com")
 
 if (api_key = Rails.configuration.x.posthog.key).present?
   PostHog.configure do |config|
@@ -8,3 +10,9 @@ if (api_key = Rails.configuration.x.posthog.key).present?
     config.host = Rails.configuration.x.posthog.host
   end
 end
+
+posthog = PostHog::Client.new({
+  api_key: api_key,
+  host: Rails.configuration.x.posthog.host,
+  on_error: Proc.new { |status, msg| print msg }
+})
